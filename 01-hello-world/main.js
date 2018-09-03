53add7462fce7eda3c9b721bdf515ae3a628db02
Deepa536 <dj.jarout@gmail.com>
2018-09-04 03:46:11 +0530

    first program

---
+++ 01-hello-world/main.js
@@ -0,0 +1,8 @@
+var myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello world!';
+
+var myHeading1 = document.querySelector('h2');
+myHeading1.textContent = 'Hello world!,it is second heading';
+
+var myHeading2 = document.querySelector('h6');
+myHeading2.textContent = 'Hello world! it is paragraph';
